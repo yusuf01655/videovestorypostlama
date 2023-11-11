4ee86503b32b1751ad96bb21a71dbde9536fcd3d
yusuf01655 <yusuf01655@outlook.com.tr>
2023-11-12 00:22:19 +0300

    mongodb baglantisi ayarlandi

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
+const mongoDbUri = "mongodb://localhost:27017/videovestorypostlama";
+mongoose.connect(mongoDbUri,{useNewUrlParser:true,});
+mongoose.connection.on("connected",() => {
+    console.log("mongodbye baglandik.");
+});
+mongoose.connection.on("error",(err) => {
+    console.log("mongodbye baglanirken hata.");
+});
+
+
+app.listen(4000, () =>{
+    console.log("uygulama port 4000de calisiyor..");
+});
