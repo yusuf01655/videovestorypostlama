0554b6eecaa9ac5da16a23672ec8d92db742ff24
yusuf01655 <yusuf01655@outlook.com.tr>
2023-11-15 15:43:27 +0300

    video yukleme icin taslak arayuz tasarlandi

--- reactt/client/src/components/UploadForm.js
+++ reactt/client/src/components/UploadForm.js
@@ -1,5 +1,21 @@
-import React from "react";
+import { BACKEND_URI } from "../config/constants";
+import React, { useState } from "react";
 const UploadForm = () => {
-    return <>UploadForm</>;
+    const [name, setName] = useState("");
+    const [videos,setVideos] = useState([]);
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+    return <><form onSubmit={handleSubmit}>
+        <div className="form-group">
+            <label htmlFor="name">isim</label>
+            <input type="text" name="name" id="name" className="form-control" onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="videos">Video yükle</label>
+                <input type="file" name="videos" id="videos" multiple className="form-control" accept=".mp4, .mkv" onChange={(e) => {setVideos(e.target.files);}}/>
+            </div>
+            <button type="submit" className="btn btn-primary mt-2">Gönder</button>
+        </form></>;
 };
 export default UploadForm;
