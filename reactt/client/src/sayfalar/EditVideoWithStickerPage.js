d4a14fa6a20046ad5be710f5906c03d1720f3214
yusuf01655 <yusuf01655@outlook.com.tr>
2024-04-22 15:04:36 +0300

    sticker ekleme kisminda resmi dondurme, boyutunu ayarlama ozelligi eklendi

--- reactt/client/src/sayfalar/EditVideoWithStickerPage.js
+++ reactt/client/src/sayfalar/EditVideoWithStickerPage.js
@@ -30,6 +30,183 @@ const EditVideoWithStickerPage = () => {
   const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
   // Add this to the top of your component
   useEffect(() => {
+    const boxWrapper = document.querySelector('.box-wrapper');
+    const rightMid = document.getElementById("right-mid");
+const leftMid = document.getElementById("left-mid");
+const topMid = document.getElementById("top-mid");
+const bottomMid = document.getElementById("bottom-mid");
+
+const leftTop = document.getElementById("left-top");
+const rightTop = document.getElementById("right-top");
+const rightBottom = document.getElementById("right-bottom");
+const leftBottom = document.getElementById("left-bottom");
+const rotate = document.getElementById("rotate");
+const box = document.getElementById('box');
+function repositionElement(x, y) {
+  boxWrapper.style.left = x + 'px';
+  boxWrapper.style.top = y + 'px';
+}
+
+function resize(w, h) {
+  box.style.width = w + 'px';
+  box.style.height = h + 'px';
+}
+    // Now you can add event listeners or perform other operations with boxWrapper
+    // Example: boxWrapper.addEventListener(...)
+    // drag support
+boxWrapper.addEventListener('mousedown', function (event) {
+  if (event.target.className.indexOf("dot") > -1) {
+      return;
+  }
+
+  initX = this.offsetLeft;
+  initY = this.offsetTop;
+  mousePressX = event.clientX;
+  mousePressY = event.clientY;
+
+
+  function eventMoveHandler(event) {
+      repositionElement(initX + (event.clientX - mousePressX),
+          initY + (event.clientY - mousePressY));
+  }
+
+  boxWrapper.addEventListener('mousemove', eventMoveHandler, false);
+  window.addEventListener('mouseup', function eventEndHandler() {
+      boxWrapper.removeEventListener('mousemove', eventMoveHandler, false);
+      window.removeEventListener('mouseup', eventEndHandler);
+  }, false);
+rightMid.addEventListener('mousedown', e => resizeHandler(e, false, false, true, false));
+leftMid.addEventListener('mousedown', e => resizeHandler(e, true, false, true, false));
+topMid.addEventListener('mousedown', e => resizeHandler(e, false, true, false, true));
+bottomMid.addEventListener('mousedown', e => resizeHandler(e, false, false, false, true));
+leftTop.addEventListener('mousedown', e => resizeHandler(e, true, true, true, true));
+rightTop.addEventListener('mousedown', e => resizeHandler(e, false, true, true, true));
+rightBottom.addEventListener('mousedown', e => resizeHandler(e, false, false, true, true));
+leftBottom.addEventListener('mousedown', e => resizeHandler(e, true, false, true, true));
+// handle rotation
+
+rotate.addEventListener('mousedown', function (event) {
+    // if (event.target.className.indexOf("dot") > -1) {
+    //     return;
+    // }
+
+    initX = this.offsetLeft;
+    initY = this.offsetTop;
+    mousePressX = event.clientX;
+    mousePressY = event.clientY;
+
+
+    var arrow = document.querySelector("#box");
+    var arrowRects = arrow.getBoundingClientRect();
+    var arrowX = arrowRects.left + arrowRects.width / 2;
+    var arrowY = arrowRects.top + arrowRects.height / 2;
+
+    function eventMoveHandler(event) {
+        var angle = Math.atan2(event.clientY - arrowY, event.clientX - arrowX) + Math.PI / 2;
+        rotateBox(angle * 180 / Math.PI);
+    }
+    console.log("boxWrapper:", boxWrapper);
+    window.addEventListener('mousemove', eventMoveHandler, false);
+
+    window.addEventListener('mouseup', function eventEndHandler() {
+        window.removeEventListener('mousemove', eventMoveHandler, false);
+        window.removeEventListener('mouseup', eventEndHandler);
+    }, false);
+}, false);
+
+}, false);
+// done drag support
+function resizeHandler(event, left = false, top = false, xResize = false, yResize = false) {
+  initX = boxWrapper.offsetLeft;
+  initY = boxWrapper.offsetTop;
+  mousePressX = event.clientX;
+  mousePressY = event.clientY;
+
+  initW = box.offsetWidth;
+  initH = box.offsetHeight;
+
+  initRotate = getCurrentRotation(boxWrapper);
+  var initRadians = initRotate * Math.PI / 180;
+  var cosFraction = Math.cos(initRadians);
+  var sinFraction = Math.sin(initRadians);
+  function eventMoveHandler(event) {
+      var wDiff = (event.clientX - mousePressX);
+      var hDiff = (event.clientY - mousePressY);
+      var rotatedWDiff = cosFraction * wDiff + sinFraction * hDiff;
+      var rotatedHDiff = cosFraction * hDiff - sinFraction * wDiff;
+
+      var newW = initW, newH = initH, newX = initX, newY = initY;
+
+      if (xResize) {
+          if (left) {
+              newW = initW - rotatedWDiff;
+              if (newW < minWidth) {
+                newW = minWidth;
+                rotatedWDiff = initW - minWidth;
+              }
+          } else {
+              newW = initW + rotatedWDiff;
+              if (newW < minWidth) {
+                newW = minWidth;
+                rotatedWDiff = minWidth - initW;
+              }
+          }
+          newX += 0.5 * rotatedWDiff * cosFraction;
+          newY += 0.5 * rotatedWDiff * sinFraction;
+      }
+
+      if (yResize) {
+          if (top) {
+              newH = initH - rotatedHDiff;
+              if (newH < minHeight) {
+                newH = minHeight;
+                rotatedHDiff = initH - minHeight;
+              }
+          } else {
+              newH = initH + rotatedHDiff;
+              if (newH < minHeight) {
+                newH = minHeight;
+                rotatedHDiff = minHeight - initH;
+              }
+          }
+          newX -= 0.5 * rotatedHDiff * sinFraction;
+          newY += 0.5 * rotatedHDiff * cosFraction;
+      }
+
+      resize(newW, newH);
+      repositionElement(newX, newY);
+  }
+
+
+  window.addEventListener('mousemove', eventMoveHandler, false);
+  window.addEventListener('mouseup', function eventEndHandler() {
+      window.removeEventListener('mousemove', eventMoveHandler, false);
+      window.removeEventListener('mouseup', eventEndHandler);
+  }, false);
+}
+function rotateBox(deg) {
+  boxWrapper.style.transform = `rotate(${deg}deg)`;
+}
+console.log("boxWrapper:", boxWrapper);
+
+
+
+// handle resize
+
+
+resize(300, 200);
+repositionElement(200, 200);
+
+
+
+
+
+
+
+///
+  }, []);
+
+  useEffect(() => {
     if (isDragging) {
       document.addEventListener('mousemove', handleMouseMove);
       document.addEventListener('mouseup', handleMouseUp);
@@ -110,6 +287,36 @@ const EditVideoWithStickerPage = () => {
     setStickerPosition({ x: localX, y: localY });
   };
 
+  ///
+  
+// var boxWrapper = document.getElementById("box-wrapper");
+const minWidth = 40;
+const minHeight = 40;
+
+
+var initX, initY, mousePressX, mousePressY, initW, initH, initRotate;
+
+
+
+
+function getCurrentRotation(el) {
+    var st = window.getComputedStyle(el, null);
+    var tm = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||
+        "none";
+    if (tm != "none") {
+        var values = tm.split('(')[1].split(')')[0].split(',');
+        var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
+        return (angle < 0 ? angle + 360 : angle); 
+    }
+    return 0;
+}
+
+
+
   return (<>
     <div>
       <h2>Edit Video with Sticker</h2>
@@ -186,7 +393,25 @@ const EditVideoWithStickerPage = () => {
       </div>
      
      
-      </div>
+      <div className="box-wrapper" id="box-wrapper">
+    <div className="box" id="box">
+        <div className="dot rotate" id="rotate"></div>
+        <div className="dot left-top" id="left-top"></div>
+        <div className="dot left-bottom" id="left-bottom"></div>
+        <div className="dot top-mid" id="top-mid"></div>
+        <div className="dot bottom-mid" id="bottom-mid"></div>
+        <div className="dot left-mid" id="left-mid"></div>
+        <div className="dot right-mid" id="right-mid"></div>
+        <div className="dot right-bottom" id="right-bottom"></div>
+        <div className="dot right-top" id="right-top"></div>
+        <div className="rotate-link"></div>
+    </div></div>
+
+
+      
+</div>
+
+
       <table>
         <th>stickerlar</th>
         <tr><td></td><img src={sticker2} alt='sticker2'/><td></td></tr>
